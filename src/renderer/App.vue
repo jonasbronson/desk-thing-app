696410f0c2e63c86b50bda3428ff687b26024813
Jonas Bronson <138813233+jonasbronson@users.noreply.github.com>
2026-01-20 15:45:47 -0600

    fix build error

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,8 +1,7 @@
 <script lang="ts">
   import { defineComponent } from 'vue'
   import InfoBar from './components/InfoBar.vue';
-  import { RouterView } from 'vue-router'
-
+  
   export default defineComponent({
     components: {
       InfoBar,
